c7fe7bb0fd40328d5dee402bfd6e13c8af36e903
haidardhiya <haidardhiyaulhaq26@gmail.com>
2021-04-02 18:10:43 +0700

    add belajar workspace

---
+++ belajar_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/computer/Desktop/hai-ros/belajar_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/computer/Desktop/hai-ros/belajar_ws/devel;/home/computer/dogkin_ws/devel;/home/computer/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/computer/Desktop/hai-ros/belajar_ws/devel;/home/computer/dogkin_ws/devel;/home/computer/catkin_ws/devel;/opt/ros/noetic' != '' else []
